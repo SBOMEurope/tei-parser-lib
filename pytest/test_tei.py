9090d1094b286290ae86ab5780d8b0811e63e842
Olle E. Johansson <oej@edvina.net>
2024-08-27 20:48:11 +0200

    Add a test of all known PURLs grabbed from the PURL type spec
    The two CPAN PURLs with :: in them are not valid according to the python
    library

--- pytest/test_tei.py
+++ pytest/test_tei.py
@@ -38,25 +38,6 @@ def test_swid(capsys, request):
     assert tvalid is True
 
 
-def test_purl01(capsys, request):
-    """Test PURL TEI."""
-    import os
-    import sys
-    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-    from lib.teiparse import valid
-
-    tei = "urn:tei:purl:prod.example.com:pkg:" \
-        "alpm/arch/containers-common@1:0.47.4-4?arch=x86_64"
-
-    tvalid = valid(tei, True)
-    captured = capsys.readouterr()
-    with capsys.disabled():
-        print(
-            "\nDEBUG {}: output: \n{}\n"
-            .format(request.node.name, captured.out))
-    assert tvalid is True
-
-
 def test_bad_00(capsys, request):
     """Test bad TEI."""
     import os
